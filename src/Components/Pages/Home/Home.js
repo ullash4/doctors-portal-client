9a78f8be5cadd7fae1f84181827137cdd22b9403
ullash4 <programmerullash@gmail.com>
2022-05-10 12:49:34 +0600

    Docappointment page setup

--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -7,7 +7,7 @@ import DocAppointment from './DocAppointment';
 
 const Home = () => {
     return (
-        <div className="px-5 md:px-6 lg:px-12">
+        <div className="px-5 md:px-6 lg:px-12 ">
             <Banner />
             <Info />
             <Service />
